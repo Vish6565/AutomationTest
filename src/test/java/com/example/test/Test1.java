ff0f96ec0b946f175bd8e9f31ee5ef17da422cb4
vishal <vishal@192.168.1.105>
2017-09-24 14:36:00 +0530

    changed code for testing at jenkins

--- src/test/java/com/example/test/Test1.java
+++ src/test/java/com/example/test/Test1.java
@@ -11,6 +11,7 @@ public class Test1 {
 		System.setProperty("webdriver.gecko.driver", "geckodriver");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.google.co.in");
+		driver.get("http://mentorstudies.com/");
 		Thread.sleep(2000);
 		driver.quit();
 	}
